e2958b6251b8ed988af2c282a68e720c11bc4801
crisG19 <110689361+crisG19@users.noreply.github.com>
2023-03-15 20:15:25 -0600

    Arreglos

--- Projecto-1/src/project/IList.java
+++
@@ -1,7 +0,0 @@
-package project;
-
-public interface IList<T> {
-	public void push(T data);
-	public T pop();
-	public int size();
-}
--- Projecto-1/src/project/Lisp.java
+++
@@ -1,7 +0,0 @@
-package project;
-
-public class Lisp {
-	//KM a Millas: KM/1.609
-	//M a ft: M*3.281
-	//cm a in: cm/2.54
-}
